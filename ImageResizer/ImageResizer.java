6175ac8a3c8f405e4e4d324a980b38a5021da71d
Redwanul (Red) Mutee <rm4243@nyu.edu>
2019-03-09 19:45:08 -0500

    added readme

--- ImageResizer/ImageResizer.java
+++ ImageResizer/ImageResizer.java
@@ -14,32 +14,32 @@ public class ImageResizer {
     //Inputs: path of base iamge, path to save new image, dimensions of square
     public void sqTransformImg(String basePath, String squarePath, int sqDim) throws IOException {
         File baseImgFile = new File(basePath);                                                             	//Open the original image file
-		BufferedImage baseImg = ImageIO.read(baseImgFile);                                                 	//Create BufferredImage for original image file
-		BufferedImage sqrImg = new BufferedImage(sqDim, sqDim, baseImg.getType());                         	//Create BufferedImage for square image with dimensions 
-		Graphics2D fillSqrImg = sqrImg.createGraphics();                                                   	//Create Graphics2D object to assist in filling square image
-		fillSqrImg.drawImage(baseImg, 0, 0, sqDim, sqDim, null);                                           	//Draw original image into square image
-		ImageIO.write(sqrImg, squarePath.substring(squarePath.lastIndexOf(".")+1), new File(squarePath));  	//Create the new image file
+	    BufferedImage baseImg = ImageIO.read(baseImgFile);                                                 	//Create BufferredImage for original image file
+	    BufferedImage sqrImg = new BufferedImage(sqDim, sqDim, baseImg.getType());                         	//Create BufferedImage for square image with dimensions 
+	    Graphics2D fillSqrImg = sqrImg.createGraphics();                                                   	//Create Graphics2D object to assist in filling square image
+	    fillSqrImg.drawImage(baseImg, 0, 0, sqDim, sqDim, null);                                           	//Draw original image into square image
+	    ImageIO.write(sqrImg, squarePath.substring(squarePath.lastIndexOf(".")+1), new File(squarePath));  	//Create the new image file
     }
     
 	public static void main(String [] args) {
         String imgFilePath;
         String sqFilePath;
-        //System.out.println(args[0]);
-        if (args.length == 0){
-            imgFilePath = "../data/ddd.jpg";
-            //Path of the default image
-            sqFilePath = "../data/squareDDD.jpg";
-            //Path of the square default image
+        int sqSideLen;
+        if (args.length == 0) {
+            imgFilePath = "../data/ddd.jpg";                                                                //Path of the default image
+            sqFilePath = "../data/squareDDD.jpg";                                                           //Path of the square default image
+            sqSideLen = 512;		                                                                        //Size of the default square image
         }
-        else{
-            // if there is more than 1 file path just do the first one
+        else {                                                                                              //User specified variables
+            System.out.println(args[0]+args[1]+args[2]);
             imgFilePath = args[0];
             sqFilePath = args[1];
+            sqSideLen = Integer.parseInt(args[2]);
         }
-		int sqSideLen = 512;                                                                               	//Size of the square image
+
 		ImageResizer ImageResizeInst = new ImageResizer();                                                 	//Instantiate class
         try { ImageResizeInst.sqTransformImg(imgFilePath, sqFilePath, sqSideLen); }                        	//Try to transform original image into square dimensions
-        catch (IOException e) { System.out.println("Error!! The File Was Not Found!!"); }
-		return ;	
+        catch (IOException e) { System.out.println("Error!! The File Was Not Found!!"); }                   //Print error otherwise
+		return;	
 	}
 }
---
+++ READ_ME.txt
@@ -0,0 +1,17 @@
+AppSec         03/08/19
+Redwanul Mutee rm4243
+Farhad Ahmed   sfa961
+
+We decided to write a short program that resizes any image to an image with specified  square dimensions. Place image to transform in the data folder. If the program is compiled without any specifications, then a default image will be transformed. Examine the data folder to view the output.
+
+To compile with default images.
+
+	javac ImageResizer.java			
+	java ImageResizer 
+
+To compile with custom images, specify via system args. 
+
+	javac ImageResizer.java			
+	java ImageResizer [-original image-] [-path-] [-size-]
+
+
